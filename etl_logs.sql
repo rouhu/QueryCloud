c6604e033df3ec1d506b86ed292374a68c6303ba
rouhu <rouhu@users.noreply.github.com>
2025-09-01 19:57:12 +0200

    Delete etl_logs.sql

--- etl_logs.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `etl_logs` (
-  `id` INT AUTO_INCREMENT PRIMARY KEY,
-  `saved_query_id` INT NOT NULL,
-  `execution_time` DATETIME NOT NULL,
-  `ended_at` DATETIME,
-  `status` VARCHAR(50) NOT NULL,
-  `message` TEXT,
-  FOREIGN KEY (`saved_query_id`) REFERENCES `saved_queries`(`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
